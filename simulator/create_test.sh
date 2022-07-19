449dc5d86de9be9d5182aa2031b4631cf414df69
George Tian <tianbeiwen88@gmail.com>
2022-07-19 19:50:09 +0800

    making sim environment independent of DISPLAY

--- simulator/create_test.sh
+++ simulator/create_test.sh
@@ -1,11 +1,9 @@
-docker run -it --rm --gpus all --name sim2real_server \
+docker run -it --rm --gpus all \
        	--network net-1 \
 	-e ROS_MASTER_URI=http://rosmaster:11311 \
 	-e DISPLAY=$DISPLAY \
         -e QT_X11_NO_MITSHM=1 \
         -e NO_AT_BRIDGE=1 \
-	-e MAGNUM_LOG=verbose \
-	-e MAGNUM_GPU_VALIDATION=on \
 	-v /dev/bus/usb:/dev/bus/usb \
         -v /dev/video0:/dev/video0 \
         -v /dev/video1:/dev/video1 \
@@ -15,4 +13,5 @@ docker run -it --rm --gpus all --name sim2real_server \
         -v /dev/video5:/dev/video5 \
         -v /tmp/.X11-unix:/tmp/.X11-unix \
 	-v /home/tb5zhh/data:/opt/workspace/src/ros_x_habitat/data \
-	sim-headless $@
+	-v /home/tb5zhh/out:/opt/workspace/output \
+	ros:noetic-ros-core $@
