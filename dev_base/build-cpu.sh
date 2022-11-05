812120c01c3528fbdffa00ea66ebfbbd5ed80d3e
George Tian <tianbeiwen88@gmail.com>
2022-11-05 23:44:54 +0800

    dev_base

--- dev_base/build-cpu.sh
+++
@@ -1,2 +0,0 @@
-# Needs torch-1.12.0+cpu-cp37-cp37m-linux_x86_64.whl
-docker build . -t dev-cpu-runtime -f Dockerfile.cpu-runtime --network host
--- dev_base/build-cuda.sh
+++
@@ -1,3 +0,0 @@
-# Needs torch-1.12.0+cu116-cp37-cp37m-linux_x86_64.whl
-docker build . -t dev-cuda-runtime -f Dockerfile.cuda-runtime --network host
-docker build . -t dev-cuda-toolkit -f Dockerfile.cuda-toolkit --network host
